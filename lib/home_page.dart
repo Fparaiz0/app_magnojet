868741a5e76500a4aad2084301ddfb134df994d5
Felipe Paraizo <fparaizo3@gmail.com>
2025-10-29 16:47:17 -0300

    add drawer at `home_page.dart` file

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'login_page.dart';
@@ -11,7 +13,17 @@ class HomePage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Página Inicial'),
+        backgroundColor: Color(0xFF15325A),
+        title: const Text(
+          'MagnoJet',
+          style: TextStyle(
+            fontFamily: 'Roboto',
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
         actions: [
           IconButton(
             icon: const Icon(Icons.logout),
@@ -25,6 +37,69 @@ class HomePage extends StatelessWidget {
             },
           ),
         ],
+      ),
+      drawer: Drawer(
+        width: 220,
+        child: Container(
+          color: Color(0xFF15325A),
+          child: Column(
+            children: [
+              Container(
+                height: 120,
+                alignment: Alignment.center,
+                padding: EdgeInsets.only(top: 50),
+                decoration: BoxDecoration(
+                  color: Color(0xFF15325A),
+                ),
+                child: Text(
+                  'Menu',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ListView(
+                  children: [
+                    ListTile(
+                      leading: Icon(Icons.home, color: Colors.white),
+                      title:
+                          Text('Home', style: TextStyle(color: Colors.white)),
+                      onTap: () => Navigator.pop(context),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.settings, color: Colors.white),
+                      title: Text('Configurações',
+                          style: TextStyle(color: Colors.white)),
+                      onTap: () => Navigator.pop(context),
+                    ),
+                  ],
+                ),
+              ),
+              const Divider(color: Colors.white24, thickness: 1),
+              ListTile(
+                leading: const Icon(Icons.logout, color: Colors.redAccent),
+                title: const Text(
+                  'Sair',
+                  style: TextStyle(
+                      color: Colors.redAccent, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                onTap: () async {
+                  await FirebaseAuth.instance.signOut();
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()),
+                    (Route<dynamic> route) => false,
+                  );
+                },
+              ),
+              const SizedBox(height: 10),
+            ],
+          ),
+        ),
       ),
       body: Center(
         child: Text(
